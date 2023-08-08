162caf18710ea1df656ab81b001fb42c6da8fec3
Christasber <christasber@yahoo.com>
2023-08-08 15:34:23 -0400

    commiting

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,10 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
-    const { budget } = useContext(AppContext);
+    
+
+    const [setBudget] = useState('');
+    const { budget } = useContext(AppContext) + setBudget;
+
     return (
         <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
+            <span>Budget:<input
+                        required='required'
+                        type='number'
+                        id='budget'
+                        value={budget}
+                        style={{ marginLeft: '2rem' , size: 10}}
+                        onChange={(event) => setBudget(event.target.value)}>
+                        </input> </span>
         </div>
     );
 };
