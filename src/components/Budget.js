3e297f6c13ef9c4e81bcba7c314943867d9eef74
Christasber <Christasber@yahoo.com>
2023-08-14 12:10:34 -0400

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,15 +1,21 @@
 
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
     const { budget } = useContext(AppContext);
+    const { dispatch,remaining  } = useContext(AppContext);
+    function handleBudgetChange(){
+        if (budget <= 20000)
+        {if (budget >= 2000){
+        dispatch({
+            type: "SET_BUDGET",
+            payload: budget,
+        })}
+        else {alert ("Budget Cannot Be Below 2000")}}
+        else {alert ("Budget Exceed 20000")}
     return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
-};
-export default Budget;
+       
             <span>Budget: 
                 <input
                     
@@ -17,13 +23,18 @@ export default Budget;
                     required
                     type='number'
                     id='budget'
-                    value={setnewBudget}
+                    value={budget}
                     style={{ marginLeft: '2rem' }}
                     onChange={handleBudgetChange()}
                 />
             </span>
-        </div>
+            
+            );
+             }
+            
+        };
     
-};
+    
+
 
 export default Budget;
